9f57daf8a18c218e1806186483aa764faf8b1034
Dani Bar-Lavi <danibarlavi@gmail.com>
2024-06-01 18:17:03 -0500

    Updated app

--- static/js/app.js
+++ static/js/app.js
@@ -3,21 +3,24 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let resultArray = metadata.filter(sampleObj.id == sample);
+    let result = resultArray[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("")
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
-  });
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  }).catch(error => console.log(error));
 }
 
 // function to build both charts
@@ -25,30 +28,66 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
 
 
     // Build a Bubble Chart
+    let bubchartData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+      }
+    }];
 
+    let bubchartLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      margin: {t: 30},
+      hovermode: 'closest',
+      xaxis: {title: 'OTU ID'},
+      margin: {t:30}
+    }
 
     // Render the Bubble Chart
-
+    Plotly.newPlot('bubble', bubchartData, bubchartLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let bar_yticks = otu_ids.slice(0,10).map(otuID => 'OTU ${otuID}').reverse();
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let barchartData = [{
+      y: bar_yticks,
+      x: sample_values.slice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
+      type: 'bar',
+      orientation: 'h'
+    }];
+
+
+    let barchartLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: {t:30, l:150}
+    };
 
 
     // Render the Bar Chart
+    Plotly.newPlot('bar', barchartData, barchartLayout);
 
-  });
+  }).catch(error => console.log(error));
 }
 
 // Function to run on page load
@@ -56,28 +95,36 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    let dropdown = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    names.forEach((name) => {
+      dropdown.append("option")
+              .text(name)
+              .property("value", name);
+    });
 
     // Get the first sample from the list
-
+    let first = names[0];
 
     // Build charts and metadata panel with the first sample
+    buildMetadata(first)
+    buildCharts(first)
 
-  });
+  }).catch(error => console.log(error));
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample)
+  buildCharts(newSample)
 }
 
 // Initialize the dashboard
